f3d8429f9a9f69d264f0232c22bb46032c4994b5
littletrainee <arechery.a1111@gmail.com>
2022-10-28 11:36:11 +0800

    Create Slices Operate Module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/littletrainee/slices
+
+go 1.19
---
+++ slices.go
@@ -0,0 +1,21 @@
+package slices
+
+// check slice is contains element.
+func ContainsElement(element any, targetslice ...any) bool {
+	for _, v := range targetslice {
+		if v == element {
+			return true
+		}
+	}
+	return false
+}
+
+// find target element's index of element.
+func FindIndexOfElement(element any, targetslice ...any) int {
+	for i, v := range targetslice {
+		if v == element {
+			return i
+		}
+	}
+	return -1
+}
