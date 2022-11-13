13a45ffec42c51a14d7af88c51649e50f86a9748
littletrainee <arechery.a1111@gmail.com>
2022-11-13 23:32:42 +0800

    Add RemoveDuplicate Function

--- slices.go
+++ slices.go
@@ -37,3 +37,15 @@ func CountNumber[T Type](targetslice []T, element T) int {
 	}
 	return count
 }
+
+func RemoveDuplicate[T Type](sliceList []T) []T {
+	allKeys := make(map[T]bool)
+	list := []T{}
+	for _, item := range sliceList {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
